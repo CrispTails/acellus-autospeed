6cdc242ec031cf7455db9d814b7bd32c9ce01a92
CrispTails <104925670+CrispTails@users.noreply.github.com>
2023-02-18 00:03:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 // ==UserScript==
 // @name          Acellus AutoSpeed
 // @namespace     http://acellus.com/
-// @match         http://acellus.com/*
+// @include       *://*.acellus.com/*
 // @grant         none
+// @description An auto video speed for Acellus.
+// @version 1.0.1
 // ==/UserScript==
+// Custom function to wait until the page is fully loaded before executing code to avoid acellus detecting this.
+function waitForPageLoad(callback) {
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener('load', callback);
+  }
+}
 
-var vids = document.getElementsByTagName("video");
+
+waitForPageLoad(function() {
+  var vids = document.getElementsByTagName("video");
  for (var i = 0; i < vids.length; i++) {
      vids[i].playbackRate = 1.5;
  }
+});
